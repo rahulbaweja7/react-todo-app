772b393662ee300fdc5822e0d5dd062b859b4dd0
“rahulbaweja7” <rahulbaweja2004@gmail.com>
2023-06-14 12:15:06 +0530

    Varun changes

--- src/component/Pagination.tsx
+++ src/component/Pagination.tsx
@@ -11,6 +11,7 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }: Props) => {
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pageNumbers.push(i);
+    // console.log("RAHUL IS THE BEST")
   }
 
   return (
