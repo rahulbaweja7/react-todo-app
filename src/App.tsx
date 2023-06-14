346654535453ab637b3db185247f0a8878ce1b53
“rahulbaweja7” <rahulbaweja2004@gmail.com>
2023-06-14 12:09:42 +0530

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import InputField from "./component/InputField";
+import TodoList from "./component/Todolist";
+import { Todo } from "./model";
+import axios from "axios";
+import Pagination from "./component/Pagination";
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+  const [error, setError] = useState({});
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([
+        ...todos,
+        { id: Date.now(), name: todo, isDone: false, avatar: "" },
+      ]);
+      setTodo("");
+    }
+  };
+  console.log(todos);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      // const res = await axios.get('https://63220eac362b0d4e7dc7c9db.mockapi.io/api/v1/todos?skip=${skip}&limit=10');
+      const res = await axios.get(
+        "https://63220eac362b0d4e7dc7c9db.mockapi.io/api/v1/todos"
+      );
+      setTodos(res.data);
+    };
+    console.log("RERENDERING");
+    const delay = 1000; // Adjust this delay as needed
+    const timer = setTimeout(fetchPosts, delay);
+    return () => clearTimeout(timer); // Clear the timeout when the component unmounts
+  }, []);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = todos.slice(indexOfFirstPost, indexOfLastPost);
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">To Do</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <TodoList todos={todos.slice(0, skip + 10)} setTodos={setTodos} /> */}
+      <TodoList todos={currentPosts} setTodos={setTodos} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={todos.length}
+        paginate={paginate}
+      />
     </div>
   );
-}
+};
 
 export default App;
